737f6834edb54c6b1fdf5a450794c6e4775aa9db
Titilayo Olorunsaiye <titilayoolorunsaiye@Titilayos-MBP.home>
2020-11-22 14:19:38 +0000

    Improved app functionality/efficiency

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ class Operations extends React.Component {
 			prevProps.lhs !== this.props.lhs ||
 			prevProps.rule !== this.props.rule
 		) {
-			this.handleOperation(prevProps.rule);
+			this.handleOperation(prevProps);
 		}
 	}
 
@@ -65,37 +65,39 @@ class Operations extends React.Component {
 			this.setState({
 				result: 0,
 			});
-		} else if (prev === "=") {
+		} else if (prev.rule === "=") {
 			this.setState({
 				result: this.state.result,
 			});
-		} else if (prev === " " || prev === "AC") {
+		} else if (prev.rule === " " || prev.rule === "AC") {
 			this.setState({
 				result: this.props.lhs,
 			});
-		} else if (prev === "+") {
+		} else if (prev.rule === "+") {
 			console.log(15 / 5);
 			this.setState({
 				result: this.state.result + this.props.lhs + this.props.rhs,
 			});
-		} else if (prev === "*") {
+		} else if (prev.rule === "*") {
 			this.setState({
 				result: this.state.result * this.props.lhs * this.props.rhs,
 			});
-		} else if (prev === "-") {
+		} else if (prev.rule === "-") {
 			this.setState({
 				result: this.state.result - this.props.lhs - this.props.rhs,
 			});
-		} else if (prev === "/") {
+		} else if (prev.rule === "/") {
 			this.setState({
 				result: this.state.result / this.props.lhs / this.props.rhs,
 			});
 		} else {
 		}
+		//this.props.dispFunc(this.state.result);
 	};
 
 	render() {
 		if (this.props.rule === "=") {
+			//this.props.dispFunc(this.state.result);
 			return <p>{this.state.result}</p>;
 		} else {
 			return <p>{this.props.disp[this.props.disp.length - 1]}</p>;
@@ -111,14 +113,7 @@ class Calculator extends React.Component {
 	render() {
 		let digits;
 		digits = digitArr.map((dig) => {
-			return (
-				<Digit
-					getter={this.props.get}
-					digit={dig}
-					currDisp={this.props.curr}
-					opr={this.props.op}
-				/>
-			);
+			return <Digit getter={this.props.get} digit={dig} />;
 		});
 
 		return <div className="calc">{digits}</div>;
@@ -152,7 +147,7 @@ class App extends React.Component {
 					this.state.operation === "+" || this.state.operation === "-"
 						? 0
 						: 1,
-				value: " ",
+				value: 0,
 				operation: input,
 			});
 		} else if (input === "AC") {
@@ -165,18 +160,33 @@ class App extends React.Component {
 				temp = parseFloat(this.state.value);
 				this.setState({
 					left: temp,
-					value: " ",
-					right: input === "+" ? 0 : 1,
+					value: input === "+" || input === "-" ? 0 : 1,
+					//right: input === "+" || input === "-" ? 0 : 1,
+					right:
+						this.state.operation === "*" ||
+						this.state.operation === "/"
+							? 1
+							: 0,
+					operation: input,
 				});
 			}
-			this.setState({
-				operation: input,
-			});
 		} else {
-			temp = this.state.value.toString() + input.toString();
-			this.setState({
-				value: temp,
-			});
+			if (
+				Number.isInteger(
+					parseInt(this.state.display[this.state.display.length - 1])
+				) ||
+				this.state.display[this.state.display.length - 1] === "."
+			) {
+				temp = this.state.value.toString() + input.toString();
+				this.setState({
+					value: temp,
+				});
+			} else {
+				temp = input.toString();
+				this.setState({
+					value: temp,
+				});
+			}
 		}
 	};
 
@@ -184,17 +194,27 @@ class App extends React.Component {
 		let temp = this.state.display;
 
 		if (this.state.operation === "=" && Number.isInteger(val)) {
-			this.setState({ display: val.toString() });
+			this.setState({
+				display: val.toString(),
+			});
+			console.log("hi");
+			//this.state.display.toString() +
 		} else {
 			if (
-				(!Number.isInteger(val) &&
-					temp.indexOf(val) !== temp.length - 1) ||
-				Number.isInteger(val)
+				Number.isInteger(val) ||
+				Number.isInteger(parseInt(temp[temp.length - 1]))
 			) {
 				let store = this.state.display.toString() + val.toString();
 				this.setState({
 					display: store,
 				});
+			} else {
+				if (!Number.isInteger(temp[temp.length - 1])) {
+					temp = temp.slice(0, temp.length - 1) + val.toString();
+					this.setState({
+						display: temp,
+					});
+				}
 			}
 		}
 	};
