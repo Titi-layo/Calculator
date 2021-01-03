1e411741849b7d0ccf9de8b7774b8fd209280049
Titilayo Olorunsaiye <titilayoolorunsaiye@Titilayos-MBP.home>
2021-01-03 23:33:37 +0000

    enabled operations on negative numbers

--- src/App.js
+++ src/App.js
@@ -4,41 +4,63 @@ import "./App.css";
 import { render } from "react-dom";
 
 let digitArr = [
-	0,
-	1,
-	2,
-	3,
-	4,
-	5,
-	6,
-	7,
-	8,
-	9,
-	"AC",
-	"+",
-	"-",
-	"*",
-	"/",
-	".",
-	"=",
+	{ id: "zero", num: 0 },
+	{ id: "one", num: 1 },
+	{ id: "two", num: 2 },
+	{ id: "three", num: 3 },
+	{ id: "four", num: 4 },
+	{ id: "five", num: 5 },
+	{ id: "six", num: 6 },
+	{ id: "seven", num: 7 },
+	{ id: "eight", num: 8 },
+	{ id: "nine", num: 9 },
+	{ id: "add", num: "+" },
+	{ id: "subtract", num: "-" },
+	{ id: "multiply", num: "*" },
+	{ id: "divide", num: "/" },
+	{ id: "decimal", num: "." },
+	{ id: "clear", num: "AC" },
+	{ id: "equals", num: "=" },
 ];
 
 class Digit extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			styling: { backgroundColor: "green" },
+		};
 	}
 
 	handleClick = () => {
 		this.props.getter(this.props.digit);
 	};
 
+	handleHover = () => {
+		if (this.state.styling.backgroundColor === "green") {
+			this.setState({
+				styling: { backgroundColor: "yellowgreen", color: "white" },
+			});
+		} else {
+			this.setState({
+				styling: { backgroundColor: "green" },
+			});
+		}
+	};
+
 	render() {
 		return (
-			<div className="digit">
-				<button onClick={this.handleClick} className="btn">
+			<React.Fragment>
+				<button
+					onClick={this.handleClick}
+					className="btn"
+					id={this.props.id}
+					onMouseEnter={this.handleHover}
+					style={this.state.styling}
+					onMouseLeave={this.handleHover}
+				>
 					{this.props.digit}
 				</button>
-			</div>
+			</React.Fragment>
 		);
 	}
 }
@@ -50,8 +72,9 @@ class Operations extends React.Component {
 			result: 0,
 		};
 	}
-
+	//|| prevProps.operation !== this.props.operation
 	componentDidUpdate(prevProps, prevState) {
+		console.log("byeee");
 		if (prevProps.lhs !== this.props.lhs) {
 			this.handleOperation(prevProps);
 		} else {
@@ -59,7 +82,7 @@ class Operations extends React.Component {
 				this.props.rule === "=" &&
 				prevState.result !== this.state.result
 			) {
-				this.props.dispFunc(this.state.result);
+				this.props.dispFunc(this.state.result.toString());
 			}
 		}
 	}
@@ -73,7 +96,7 @@ class Operations extends React.Component {
 			this.setState({
 				result: this.state.result,
 			});
-		} else if (prev.rule === " " || prev.rule === "AC") {
+		} else if (prev.rule === "" || prev.rule === "AC") {
 			this.setState({
 				result: this.props.lhs,
 			});
@@ -111,14 +134,14 @@ class Calculator extends React.Component {
 		super(props);
 		this.state = {
 			left: 0,
-			value: " ",
-			operation: " ",
+			value: "",
+			operation: "",
 		};
 	}
 
 	handleInput = (input) => {
 		let temp = this.props.displ.toString();
-		if (input === "=" && this.state.value) {
+		if (input === "=" && this.state.value !== "") {
 			this.setState({
 				left: parseFloat(this.state.value),
 				value: 0,
@@ -127,35 +150,72 @@ class Calculator extends React.Component {
 		} else if (input === "AC") {
 			this.setState({
 				operation: input,
-				value: " ",
+				value: "",
 			});
-			this.props.disp(" ");
+			this.props.disp(0);
 		} else if (!Number.isInteger(input) && input !== ".") {
-			if (this.state.value !== " ") {
-				this.props.disp(this.props.displ.toString() + input.toString());
+			//&& !Number.isInteger(temp[temp.length - 1])
 
+			if (this.state.value !== "" && this.state.value !== "-") {
+				this.props.disp(this.props.displ.toString() + input.toString());
 				this.setState({
 					left: parseFloat(this.state.value),
-					value: " ",
+					value: "",
 					operation: input,
 				});
-			} else {
-				if (temp.length > 1) {
-					console.log(temp.length);
-					temp = temp.slice(0, temp.length - 1);
-					this.props.disp(temp + input);
+			} else if (temp.length > 1) {
+				//!Number.isInteger(temp[temp.length - 1])
+				// && Number.isInteger(pareseInt(temp[temp.lenth - 1]))
+				if (input === "-" && this.state.value !== "-") {
 					this.setState({
-						operation: input,
+						value: this.state.value.toString() + input.toString(),
 					});
+					this.props.disp(
+						this.props.displ.toString() + input.toString()
+					);
+				} else {
+					let text;
+
+					if (this.state.value === "-" && input !== "-") {
+						text = /\D+$/;
+					} else {
+						text = /\D$/;
+					}
+
+					temp = temp.replace(text, input);
+
+					this.props.disp(temp);
+
+					this.setState({
+						operation: input !== "-" ? input : this.state.operation,
+						value: input === "-" ? "-" : "",
+					});
+				}
+			} else {
+				if (input === "+" || input === "-") {
+					this.setState({
+						value: input.toString(),
+					});
+					this.props.disp(input.toString());
+					// this.props.displ.toString() +
+					//this.state.value.toString() +
 				}
 			}
 		} else {
 			if (this.state.value === 0) {
 				this.setState({
 					value: input.toString(),
-					operation: " ",
+					operation: "",
 				});
 				this.props.disp(input.toString());
+			} else if (input === "." && this.state.value.includes(".")) {
+			} else if (this.state.value === "0" || this.props.displ === 0) {
+				this.setState({
+					value: input.toString(),
+				});
+
+				temp = temp.slice(0, temp.length - 1);
+				this.props.disp(temp + input.toString());
 			} else {
 				this.setState({
 					value: this.state.value.toString() + input.toString(),
@@ -168,14 +228,15 @@ class Calculator extends React.Component {
 	render() {
 		let digits;
 		digits = digitArr.map((dig) => {
-			return <Digit getter={this.handleInput} digit={dig} />;
+			return (
+				<Digit getter={this.handleInput} digit={dig.num} id={dig.id} />
+			);
 		});
 
 		return (
 			<React.Fragment>
 				<Operations
 					lhs={this.state.left}
-					rhs={this.state.right}
 					rule={this.state.operation}
 					dispFunc={this.props.disp}
 				/>
@@ -189,7 +250,7 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			display: " ",
+			display: 0,
 		};
 	}
 
